3766ba3b8194de376b74d60f6a5424d1efc020ea
ZackHu <67856327+Zack-Hu-Nottingham@users.noreply.github.com>
2022-01-26 14:33:42 +0800

    Support multilanguage

---
+++ Team01/language/ch_language.js
@@ -0,0 +1,45 @@
+var Languague = {
+  // 首页
+   index: {
+     message_page: "消息",
+     project_page: "项目",
+     dashboard_page: "首页",
+     more_page: "用户",
+
+     // message page
+     notification: "通知",
+     recent_message: "最近的消息",
+
+     // project page
+     my_task: "我的任务",
+     statistic_report: "数据报告",
+     project_list: "项目列表",
+     create_new_project: "新建项目",
+
+     // dashboard page
+     today_task: "今天的任务",
+
+     // more page
+     user_name: "用户名",
+     position: "职位",
+     setting: "设置",
+     more: "更多",
+     sign_out: "登出",
+     invite_member: "邀请用户",
+     language: "语言",
+     more: "更多信息",
+
+     // language setting page
+     current_lan: "当前语言",
+     available_lan: "可选语言",
+     choose: "选择",
+     success_change: "切换成功",
+     change_lan_confirm: "确认切换语言",
+
+
+   },
+};
+
+module.exports = {
+   lang: Languague
+}
---
+++ Team01/language/en_language.js
@@ -0,0 +1,46 @@
+var Languague = {
+  // 首页
+  index: {
+    message_page: "Message",
+    project_page: "Projects",
+    dashboard_page: "Dashboard",
+    more_page: "More",
+
+    // message page
+    notification: "Notification",
+    recent_message: "Recent Message",
+
+    // project page
+    my_task: "My Task",
+    statistic_report: "Statistics Reort",
+    project_list: "Projects",
+    create_new_project: "Create New Project",
+
+    // dashboard page
+    today_task: "Today's Task",
+
+    // more page
+    user_name: "User name",
+    position: "Possition",
+    setting: "Setting",
+    more: "More",
+    sign_out: "Sign Out",
+    invite_member: "Invite Member",
+    language: "Language",
+    more: "More Information",
+
+     // language setting page
+    current_lan: "Current language",
+    available_lan: "Available language",
+    choose: "Choose",
+    success_change: "Change successful",
+    change_lan_confirm: "Confirm change language",
+    
+
+    
+ },
+};
+
+module.exports = {
+   lang: Languague
+}
