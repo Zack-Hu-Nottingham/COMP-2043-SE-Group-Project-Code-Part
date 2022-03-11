30f16ebdfd023c5e62fb32d6ca644116760fac6a
MingKai <scymk1@nottingham.edu.cn>
2022-03-11 14:43:02 +0800

    correct name Input

--- Team01/pages/indexs/indexForProjectManager/indexForProjectManager.js
+++ Team01/pages/indexs/indexForProjectManager/indexForProjectManager.js
@@ -393,7 +393,6 @@ Page({
    */
   forNotice: function (e) {
     let value = this.data.value;
-    var id = app.globalData.userInfo._openid;
     if (value == '') {
       Toast.fail('空用户名');
     } else {
