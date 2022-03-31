ee1b96d109bfba34a4eba9266d719fc6fb4c19e3
edison <scyyz11@nottingham.edu.cn>
2022-03-31 13:35:33 +0800

    Update comment to each file

--- Team01/cloud/updateData/index.js
+++ Team01/cloud/updateData/index.js
@@ -1,3 +1,8 @@
+/*
+ * Code written by team
+ * Code created by: Yuhong Wei
+ * Code Modified by: Yuhong Wei
+ */
 // 云函数入口文件
 const cloud = require('../updateTaskDate/node_modules/wx-server-sdk')
 
--- Team01/cloud/updateProjectDescription/index.js
+++ Team01/cloud/updateProjectDescription/index.js
@@ -1,3 +1,8 @@
+/*
+ * Code written by team
+ * Code created by: Yuhong Wei
+ * Code Modified by: Yuhong Wei
+ */
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
